b24130757d72d263004fd156eb73a0e04841a93d
Jonathan Picazo <jpica003@ucr.edu>
2020-12-04 06:37:10 -0800

    finished part 2

--- part2/client.py
+++ part2/client.py
@@ -9,16 +9,16 @@ import time
 Function Definition
 '''
 def receiveThread(s):
-	while True:
-		try:
-			reply = s.recv(4096) # receive msg from server
-			# You can add operations below once you receive msg
-			# from the server
+    while True:
+        try:
+            reply = s.recv(4096) # receive msg from server
+            # You can add operations below once you receive msg
+            # from the server
 
-		except:
-			print "Connection closed"
-			break
-	
+        except:
+            print "Connection closed"
+            break
+    
 
 def tupleToString(t):
 	s = ""
@@ -61,16 +61,8 @@ Connect to remote server
 s.connect((remote_ip , port))
 print 'Socket Connected to ' + host + ' on ip ' + remote_ip
 
-'''
-TODO: Part-1.1, 1.2: 
-Enter Username and Passwd
-'''
-welcMsg = s.recv(1024)
-print welcMsg
 
-# Whenever a user connects to the server, they should be asked for their username and password.
-# Username should be entered as clear text but passwords should not (should be either obscured or hidden). 
-# get username from input. HINT: raw_input(); get passwd from input. HINT: getpass()
+messageQueue = []
 
 print 'Enter username: '
 username = raw_input()
@@ -82,131 +74,153 @@ s.sendall(username + '<>' + passwd)
 #msgBack = s.recv(1024)
 
 # Start of the Skeleton Code
-
 reply = s.recv(5)
 if reply == 'valid':
-	print 'Username and password valid'
-	ss = s.recv(4096)
-	'''
-	Part-2:TODO: Please printout the number of unread message once a new client login
-	'''
-	start_new_thread(receiveThread, (s,))
-	message = ""
-	while True :	
-		message = raw_input("Choose an option (type the number): \n 1. Logout \n 2. Send messages \n 3. Group Configuration \n 4. Offline message \n")		
-		try :
-			s.sendall(message)
-			if message == str(1):
-				break
-				
-			if message == str(2):
-				while True:
-					message = raw_input("Choose an option (type the number): \n 1. Private messages \n 2. Broadcast messages \n 3. Group messages \n")
-					try :
-						'''
-						Part-2:TODO: Send option to server
-						'''
-						if message == str(1):
-							pmsg = raw_input("Enter your private message\n")
-							try :
-								'''
-								Part-2:TODO: Send private message
-								'''
-							except socket.error:
-								print 'Private Message Send failed'
-								sys.exit()
-							rcv_id = raw_input("Enter the recevier ID:\n")
-							try :
-								'''
-								Part-2:TODO: Send private message
-								'''
-								break
-							except socket.error:
-								print 'rcv_id Send failed'
-								sys.exit()
-						if message == str(2):
-							bmsg = raw_input("Enter your broadcast message\n")
-							try :
-								'''
-								Part-2:TODO: Send broadcast message
-								'''
-								break
-							except socket.error:
-								print 'Broadcast Message Send failed'
-								sys.exit()
-						if message == str(3):
-							gmsg = raw_input("Enter your group message\n")
-							try :
-								'''
-								Part-2:TODO: Send group message
-								'''
-							except socket.error:
-								print 'Group Message Send failed'
-								sys.exit()
-							g_id = raw_input("Enter the Group ID:\n")
-							try :
-								'''
-								Part-2:TODO: Send group message
-								'''
-								break
-							except socket.error:
-								print 'g_id Send failed'
-								sys.exit()
-					except socket.error:
-						print 'Message Send failed'
-						sys.exit() 
-					
-			if message == str(3):
-				option = raw_input("Do you want to: 1. Join Group 2. Quit Group: \n")
-				if option == str(1):
-					group = raw_input("Enter the Group you want to join: ")
-					try :
-						'''
-						Part-2:TODO: Join a particular group
-						'''
-					except socket.error:
-						print 'group info sent failed'
-						sys.exit()
-				elif option == str(2):
-					group = raw_input("Enter the Group you want to quit: ")
-					try :
-						'''
-						Part-2:TODO: Quit a particular group
-						'''
-					except socket.error:
-						print 'group info sent failed'
-						sys.exit()
-				else:
-					print 'Option not valid'
-			
-			if message == str(4):
-				while not os.getpgrp() == os.tcgetpgrp(sys.stdout.fileno()):
-					pass
-				option = raw_input("Do you want to: 1. View all offline messages; 2. View only from a particular Group\n")
-				if option == str(1):					
-					try :
-						'''
-						Part-2:TODO: View all offline messages
-						'''
-					except socket.error:
-						print 'msg Send failed'
-						sys.exit()
-				elif option == str(2):
-					group = raw_input("Enter the group you want to view the messages from: ")
-					try :
-						'''
-						Part-2:TODO: View only from a particular group
-						'''
-					except socket.error:
-						print 'group Send failed'
-						sys.exit()
-				else:
-					print 'Option not valid'
-		except socket.error:
-			print 'Send failed'
-			sys.exit()
-		
+    print 'Username and password valid'
+    ss = s.recv(1)
+    print 'You have ' + ss + ' unread messages'
+    # receive user messages here, does not work past start_new_thread line for some reason :(
+    if ss != '0':
+        messageBuff = s.recv(4096)
+        messList = messageBuff.split(':')
+        messList = messList[:-1] # deletes empty array element
+        for i in range(0, len(messList)):
+            newList = messList[i].split('<>') # splits string into sender and message
+            toAdd = newList[0] + ': ' + newList[1]
+            messageQueue.append(toAdd)
+        
+
+    start_new_thread(receiveThread, (s,))
+    message = ""
+    while True :	
+        message = raw_input("Choose an option (type the number): \n 1. Logout \n 2. Send messages \n 3. Group Configuration \n 4. Offline message \n")		
+        try :
+            s.sendall(message)
+            if message == str(1):
+                break
+            if message == str(2):
+                while True:
+                    message2 = raw_input("Choose an option (type the number): \n 1. Private messages \n 2. Broadcast messages \n 3. Group messages \n")
+                    try:
+                        s.sendall(message2)
+                        if message2 == str(1):
+                            pmsg = raw_input("Enter your private message\n")
+                            try:
+                                # send private message
+                                s.sendall(pmsg)
+                            except socket.error:
+                                print 'Private Message Send failed'
+                                sys.exit()
+                            rcv_id = raw_input("Enter the recevier ID:\n")
+                            try :
+                                # send recipient id
+                                s.sendall(rcv_id)
+                                break
+                            except socket.error:
+                                print 'rcv_id Send failed'
+                                sys.exit()
+                        if message2 == str(2):
+                            bmsg = raw_input("Enter your broadcast message\n")
+                            try :
+                                '''
+                                Part-2:TODO: Send broadcast message
+                                '''
+                                s.sendall(bmsg)
+                                break
+                            except socket.error:
+                                print 'Broadcast Message Send failed'
+                                sys.exit()
+                        if message2 == str(3):
+                            gmsg = raw_input("Enter your group message\n")
+                            try :
+                                '''
+                                Part-2:TODO: Send group message
+                                '''
+                                s.sendall(gmsg)
+                            except socket.error:
+                                print 'Group Message Send failed'
+                                sys.exit()
+                            g_id = raw_input("Enter the Group ID:\n")
+                            try :
+                                '''
+                                Part-2:TODO: Send group message
+                                '''
+                                s.sendall(g_id)
+                                break
+                            except socket.error:
+                                print 'g_id Send failed'
+                                sys.exit()
+                    except socket.error:
+                        print 'Message Send failed'
+                        sys.exit() 
+                    
+            if message == str(3):
+                option = raw_input("Do you want to: 1. Join Group 2. Quit Group 3. List Groups: \n")
+                s.sendall(option)
+                if option == str(1):
+                    group = raw_input("Enter the Group you want to join: ")
+                    try :
+                        '''
+                        Part-2:TODO: Join a particular group
+                        '''
+                        s.sendall(group)
+                    except socket.error:
+                        print 'group info sent failed'
+                        sys.exit()
+                elif option == str(2):
+                    group = raw_input("Enter the Group you want to quit: ")
+                    try :
+                        '''
+                        Part-2:TODO: Quit a particular group
+                        '''
+                        s.sendall(group)
+                    except socket.error:
+                        print 'group info sent failed'
+                        sys.exit()
+                elif option == str(3):
+                    print 'Here are the available groups: '
+                    print 'group1'
+                    print 'group2'
+                    print 'group3'
+                else:
+                    print 'Option not valid'
+            
+            if message == str(4):
+                while not os.getpgrp() == os.tcgetpgrp(sys.stdout.fileno()):
+                    pass
+                option = raw_input("Do you want to: 1. View all offline messages; 2. View only from a particular Group\n")
+                isread = 0
+                s.sendall(option)
+                if option == str(1):					
+                    try :
+                        '''
+                        Part-2:TODO: View all offline messages
+                        '''
+                        for i in range(0, len(messageQueue)):
+                            isread += 1
+                            print messageQueue[i]
+                        s.sendall(str(isread))
+                    except socket.error:
+                        print 'msg Send failed'
+                        sys.exit()
+                elif option == str(2):
+                    group = raw_input("Enter the group you want to view the messages from: ")
+                    try :
+                        for i in range(0, len(messageQueue)):
+                            if group in messageQueue[i]:
+                                isread += 1
+                                print messageQueue[i]
+                        s.sendall(str(isread))
+                    except socket.error:
+                        print 'group Send failed'
+                        sys.exit()
+                        
+                else:
+                    print 'Option not valid'
+        except socket.error:
+            print 'Send failed'
+            sys.exit()		
 else:
-	print 'Invalid username or password'
+    print 'Invalid username or password'
 
 s.close()
--- part2/server.py
+++ part2/server.py
@@ -17,19 +17,13 @@ def tupleToString(t):
 def stringToTuple(s):
 	t = s.split("<>")
 	return t
-
+# returns user index of array of arrays
 def findUser(search ,myList):
 	for i in range(0, len(myList)):
 		if myList[i][0] == search:
 			return i
 	return -1
 
-def getMessages(user, myList):
-	retString = ''
-	size = len(myList[user])
-	for i in range(0, size):
-		retString += myList[user][i]
-	return retString
 '''
 Create Socket
 '''
@@ -60,24 +54,58 @@ print 'Socket now listening'
 clients = []
 userpass = [["user1", "passwd1"], ["user2", "passwd2"], ["user3", "passwd3"]]
 messages = [[],[],[]]
-subscriptions = [[],[],[]] # Store the group info
+groups = ['group1', 'group2', 'group3']
+subscriptions = [[],[],[]] # Store the group info (users in group)
+online = []
+unreadCount = [0,0,0]
+# group1, group2, group3
 
-def clientthread(conn):
+def clientThread(conn):
 	global clients
 	global count
-    global messages
-    global userpass
-
+	global messages
+	global userpass
+	global groups
+	global subscriptions
+	global online
+	global unreadCount
 	uppair = conn.recv(1024)
 	uppair = stringToTuple(uppair)
+	username = ''
 	if uppair in userpass:
 		user = userpass.index(uppair)
+		username = userpass[user][0]
+		online.append(username)
+
+		try:
+			conn.sendall('valid')
+		except socket.error:
+			print 'Send failed'
+			sys.exit()
 		
 		'''
 		Part-2:TODO: 
 		After the user logs in, check the unread message for this user.
 		Return the number of unread messages to this user.
 		'''
+		# Send messages to user here so it doesnt mess up buffer
+		try:
+			# conn.sendall(str(len(messages[user])))
+			a = str(unreadCount[user])
+			conn.sendall(a)
+		except socket.error:
+			print 'Send failed'
+			sys.exit()
+		pr = ''
+		for i in range(0, len(messages[user])):
+			pr += tupleToString(messages[user][i]) + ':'
+
+		if len(messages[user]) != 0:
+			try:
+				conn.sendall(pr)
+			except socket.error:
+				print 'Send failed'
+				sys.exit()		
 
 		while True:
 			try :
@@ -86,7 +114,6 @@ def clientthread(conn):
 				break
 			if option == str(1):
 				# Logout that you implemented in Part-1
-                user = ''
 				break;
 			elif option == str(2):
 				message = conn.recv(1024)
@@ -94,23 +121,94 @@ def clientthread(conn):
 					'''
 					Part-2:TODO: Send private message
 					'''
-              
+					pmsg = conn.recv(1024)
+					rcv_id = conn.recv(1024)
+					addtoMsg = username + '<>' + pmsg
+					addtoMsg = stringToTuple(addtoMsg)
+					# find index of the recipient
+					recptID = findUser(rcv_id, userpass)
+					if recptID == -1:
+						# invalid id
+						break
+					# add to their message list
+					messages[recptID].append(addtoMsg)
+					print 'To: ' + userpass[recptID][0]
+					print addtoMsg
+					unreadCount[recptID] += 1
+					
 				if message == str(2):
 					'''
 					Part-2:TODO: Send broadcast message
 					'''
+					bmsg = conn.recv(1024)
+					print 'Sending broadcast msg, ' + bmsg
+					addtoMsg = 'broadcast' + '<>' + bmsg
+					addtoMsg = stringToTuple(addtoMsg)
+
+					# for i in range(0, len(messages)):
+					# 	messages[i].append(addtoMsg)
+					# 	print 'Sent to ' + userpass[i][0]
+					for i in range(0, len(online)):
+						uid = findUser(online[i], userpass)
+						messages[uid].append(addtoMsg)
+						unreadCount[uid] += 1
+						print 'sent to user with id of ' + str(uid)
 				if message == str(3):
 					'''
 					Part-2:TODO: Send group message
 					'''
+					gmsg = conn.recv(1024)
+					gid = conn.recv(1024)
+					addtoMsg = gid + '<>' + gmsg
+					addtoMsg = stringToTuple(addtoMsg)
+					idx = int(gid[len(gid) - 1]) - 1
+					print idx
+					for i in range(0, len(subscriptions[idx])):
+						toFind = subscriptions[idx][i]
+						uid = findUser(toFind, userpass)
+						if uid == -1:
+							break
+						messages[uid].append(addtoMsg)
+						unreadCount[uid] += 1
+						print 'Sent group msg to ' + toFind
+
+
 			elif option == str(3):
 				'''
 				Part-2:TODO: Join/Quit group
 				'''
+				message = conn.recv(1024)
+				#group = conn.recv(1024)
+				if message == str(1):
+					group = conn.recv(1024)
+					for i in range(0,len(groups)):
+						if groups[i] == group:
+							subscriptions[i].append(username)
+							print username + ' joined ' + groups[i]
+				if message == str(2):
+					group = conn.recv(1024)
+					for i in range(0, len(groups)):
+						if groups[i] == group:
+							subscriptions[i].remove(username)
+							print 'Removed ' + username + ' from ' + groups[i]
+				if message == str(3):
+					print 'list groups'
+					# list all groups
+
 			elif option == str(4):
 				'''
 				Part-2:TODO: Read offline message
 				'''
+				suboption = conn.recv(1024)
+				howmanyread = conn.recv(1024)
+				if suboption == str(1):
+					unreadCount[user] = 0
+				else:
+					unreadCount[user] -= int(howmanyread)
+					if unreadCount[user] < 0:
+						unreadCount[user] = 0
+				print 'Option: ' + suboption
+				
 			else:
 				try :
 					conn.sendall('Option not valid')
@@ -127,11 +225,22 @@ def clientthread(conn):
 	conn.close()
 	if conn in clients:
 		clients.remove(conn)
+	if username in online:
+		online.remove(username)
 
 def receiveClients(s):
 	# Use the code in Part-1, do some modification if necessary
-
+	global clients
+	while 1:
+		# Tips: Wait to accept a new connection (client) - blocking call
+		conn, addr = s.accept()
+		print 'Connected with ' + addr[0] + ':' + str(addr[1])
+		clients.append(conn)
+		# Tips: start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+		start_new_thread(clientThread ,(conn,))
+		
 start_new_thread(receiveClients ,(s,))
+
 while 1:
 	message = raw_input()
 	if message == 'messagecount':
@@ -148,7 +257,6 @@ while 1:
 		subscriptions.append([])
 		print 'User created'
 	elif message == 'listgroup':
-		'''
-		Part-2:TODO: Implement the functionality to list all the available groups
-		'''
+		for i in range (0, len(groups)):
+			print groups[i]
 s.close()
